072ed57d9a6b1262eabf846675c244e4ddf0fe1a
Theodoros Pilarinos <thpilarinos@novi.local>
2021-05-06 10:41:30 +0300

    Recipe List and Recipe Details components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A Test','This is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg'),
+    new Recipe('A Test','This is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg')
+
+  ];
 
   constructor() { }
 
